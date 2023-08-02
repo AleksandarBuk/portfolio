277ebb72411b177cf61a913ab7ea503e348aebf2
AleksandarBuk <aleksandar.buk@gmail.com>
2023-08-02 22:59:25 +0200

    Added Flask server for the CV mail

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-// const headerElement = document.getElementById("header");
-
-// const text = `
-//   <h1>Hello, I'm <br>Aleksandar Bukvic</br></h1>
-//   <p>Welcome to my website</p>
-// `;
-
-// headerElement.innerHTML = text;
-
 function sendDocument() {
     var email = prompt("Please enter your email:");
     if (email !== null && email.trim() !== "") {
         var filePath = "Resume-AleksandarBuk.pdf";
 
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/send-document");
+        xhr.open("POST", "/");
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onload = function () {
             if (xhr.status === 200) {
@@ -25,21 +16,23 @@ function sendDocument() {
                 console.error("Error sending document");
             }
         };
+        xhr.onerror = function () {
+            console.error("Error sending document");
+        };
         xhr.send(JSON.stringify({ email: email, filePath: filePath }));
     }
 }
 
-function opentab(tabname) {
-    for (tablink of tablinks) {
+function opentab(event, tabname) {
+    for (let tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for (tabcontent of tabcontents) {
+    for (let tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
-
 
 var sidemenu = document.getElementById("sidemenu");
 
@@ -50,19 +43,20 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px";
 }
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzP-oU2IQuh8_6J14Fy7KcRktWsTXWyQ6917b1maXX5TQGmEwd9J6LWBPDtSSOjqho/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzP-oU2IQuh8_6J14Fy7KcRktWsTXWyQ6917b1maXX5TQGmEwd9J6LWBPDtSSOjqho/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
-            msg.innerHTML = "Message sent sucessfuly"
+            msg.innerHTML = "Message sent successfully";
             setTimeout(function () {
-                msg.innerHTML = ""
-            }, 3000)
-            form.reset()
+                msg.innerHTML = "";
+            }, 3000);
+            form.reset();
         })
-        .catch(error => console.error('Error!', error.message))
-})
+        .catch(error => console.error('Error!', error.message));
+});
